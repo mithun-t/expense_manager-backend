7c2139a7d00b6f9de09fafc77bf29dbbf9aa006b
Happy Tom <mithunaksh01@gmail.com>
2024-09-30 23:55:27 +0530

    delete expense category view

--- expense_manager/settings.py
+++ expense_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8d^48@kv741lgr)dattfepr4hipvvn_*%k_s7qqxi59^1h$az0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.43']
 
 
 # Application definition
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,6 +17,11 @@ class CategoryMaster(APIView):
             serializer.save()
             return Response({'message': 'Category added'}, status=status.HTTP_201_CREATED)
         return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+   
+    def delete(self, request, pk=None):
+        category = get_object_or_404(Category, pk=pk)
+        category.delete()
+        return Response({'message': 'Category deleted'}, status=status.HTTP_204_NO_CONTENT)
 
 class ExpenseMaster(APIView):
     def get(self, request):
@@ -29,3 +35,8 @@ class ExpenseMaster(APIView):
             serializer.save()
             return Response({'message': 'Expense added'}, status=status.HTTP_201_CREATED)
         return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk=None):
+        expense = get_object_or_404(Expense, pk=pk)
+        expense.delete()
+        return Response({'message': 'Expense deleted'}, status=status.HTTP_204_NO_CONTENT)
