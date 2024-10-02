136dce2275e1f66aec886987ea28fda5f76f545e
Happy Tom <mithunaksh01@gmail.com>
2024-10-02 21:38:22 +0530

    urls updated

--- expenses/urls.py
+++ expenses/urls.py
@@ -4,4 +4,7 @@ from .views import CategoryMaster, ExpenseMaster
 urlpatterns = [
     path('categories/', CategoryMaster.as_view(), name='category-list'),
     path('expenses/', ExpenseMaster.as_view(), name='expense-list'),
+    path('categories/<int:pk>/', CategoryMaster.as_view(), name='category-detail'), 
+    path('expenses/<int:pk>/', ExpenseMaster.as_view(), name='expense-detail'),
+
 ]
